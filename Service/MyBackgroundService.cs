8900787ef5ef008f8b8a9d7c8eabc37b82278497
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-15 09:18:00 +0330

    Add project files.

---
+++ Service/MyBackgroundService.cs
@@ -0,0 +1,41 @@
+﻿namespace IHostedServiceWebAPP.Service
+{
+    using Microsoft.Extensions.Hosting;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class MyBackgroundService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Task Start");
+
+            // تنظیم یک تایمر برای اجرای وظایف دوره‌ای
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            Console.WriteLine($"Task Runnig - {DateTime.Now}");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Task Stop");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
